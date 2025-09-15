49343755a32aa26aedc59f4c5fa73f62e939d283
Ńick Stoltze <nick.stoltze0597@gmail.com>
2025-09-15 18:05:12 +0200

    still white window, but kinda starting?

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const { app, BrowserWindow } = require("electron");
-//const RED = require("node-red");
+const http = require("http");
+const express = require("express");
+const RED = require("node-red");
+
+const expressApp = express();
+const server = http.createServer(app);
+
+const settings = {
+  httpAdminRoot: "/", 
+  httpNodeRoot: "/",
+  userDir: __dirname + "/userDir",
+  flowFile: "flows.json",
+  uiPort: 1880
+};
+
 
 app.whenReady().then(() => {
     const window = new BrowserWindow({
@@ -10,7 +24,26 @@ app.whenReady().then(() => {
         }
     });
 
-    setTimeout(() => {
-        window.loadURL("http://localhost:1880/dashboard")
-    }, 4000)
+    RED.init(server, settings);
+
+    expressApp.use(settings.httpAdminRoot, RED.httpAdmin);
+    expressApp.use(settings.httpNodeRoot, RED.httpNode);
+
+    async function promiseRED(){
+        await RED.start()
+        return "Starting Node-RED..."
+    }
+
+    promiseRED().then(
+        function(val){
+            console.log(val)
+            window.loadURL("http://localhost:1880/dashboard")
+        },
+        function(err){console.log(err)}
+    )
 })
+
+
+server.listen(settings.uiPort, () => {
+  console.log(`Node-RED läuft auf http://localhost:${settings.uiPort}`);
+});
